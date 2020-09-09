440ecb36de8741b9d01f71b261a61bd56340f903
Holstrup <abho@dtu.dk>
2020-09-09 19:25:14 +0200

    Short introduction

--- run.sh
+++ run.sh
@@ -15,7 +15,7 @@
 #BSUB -gpu "num=1:mode=exclusive_process"
 
 ### -- set walltime limit: hh:mm --  maximum 24 hours for GPU-queues right now
-#BSUB -W 1:00
+#BSUB -W 4:00
 
 # request 5GB of system-memory
 #BSUB -R "rusage[mem=5GB]"
@@ -23,8 +23,7 @@
 ### -- set the email address --
 # please uncomment the following line and put in your e-mail address,
 # if you want to receive e-mail notifications on a non-default address
-##BSUB -u abho@dtu.dk
-##BSUB -u alexanderholstrup@gmail.com
+#BSUB -u abho@dtu.dk
 
 ### -- send notification at start --
 #BSUB -B
@@ -38,6 +37,24 @@
 #BSUB -e gpu_%J.err
 # -- end of LSF options --
 
-echo "Hello"
+### - Interactive environment: sxm2sh -
+
+# Load modules 
+module unload cuda
+module unload cudann
+module load cuda/9.0
+module load cudnn/v7.0.5-prod-cuda-9.0
+module load python3/3.7.5
+
+#Create a virtual environment for Python3
+python3 -m venv training_env
+
+#Activate virtual environment
+source ~/training_env/bin/activate
+
+echo "Installing Dependencies"
+pip3 install -r requirements.txt
+
+echo "Starting Training"
 python3 -m cirtorch.examples.train outputs
-echo "Goodbye"
+echo "Finished Training"
