d32c3e3c7154504c24bf6a8206c76695b75f56ae
Holstrup <abho@dtu.dk>
2020-10-26 15:00:08 +0100

    unnormalize

--- cirtorch/examples/train.py
+++ cirtorch/examples/train.py
@@ -369,12 +369,13 @@ def train(train_loader, model, criterion, optimizer, epoch):
             if i % 5 == 0:
                 print(gps_info)
                 print("Adding Images")
-                std = 0.5
-                mean = 0.5
+                mean = torch.tensor([0.485, 0.456, 0.406]).view(3,1,1)
+                std = torch.tensor([0.229, 0.224, 0.225]).view(3,1,1)
                 
                 url = "https://www.google.com/maps/dir/QUERY_POINT/POSITIVE_POINT/@QUERY_POINT,14.75z"
                 # Write Image 
                 img = torch.squeeze(input[q][0])
+                torch.mul(img, std)
                 img = ((img * std) + mean)
                 writer.add_image('Query', img, epoch)
 
