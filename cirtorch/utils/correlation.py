498ddaebb85116910f9567f147fe79edcf2beaf7
Holstrup <abho@dtu.dk>
2021-02-08 13:15:18 +0100

    new correlation file

---
+++ cirtorch/utils/correlation.py
@@ -0,0 +1,245 @@
+import argparse
+import os
+import time
+import pickle
+import pdb
+
+import numpy as np
+
+import torch
+from torch.utils.model_zoo import load_url
+from torchvision import transforms
+
+from cirtorch.networks.imageretrievalnet import init_network, extract_vectors
+from cirtorch.datasets.datahelpers import cid2filename
+from cirtorch.datasets.traindataset import TuplesDataset
+from cirtorch.datasets.testdataset import configdataset
+from cirtorch.datasets.genericdataset import ImagesFromList
+from cirtorch.utils.download import download_train, download_test
+from cirtorch.utils.whiten import whitenlearn, whitenapply
+from cirtorch.utils.evaluate import mapk, recall
+from cirtorch.utils.general import get_data_root, htime
+
+PRETRAINED = {
+    'retrievalSfM120k-vgg16-gem'        : 'http://cmp.felk.cvut.cz/cnnimageretrieval/data/networks/retrieval-SfM-120k/retrievalSfM120k-vgg16-gem-b4dcdc6.pth',
+    'retrievalSfM120k-resnet101-gem'    : 'http://cmp.felk.cvut.cz/cnnimageretrieval/data/networks/retrieval-SfM-120k/retrievalSfM120k-resnet101-gem-b80fb85.pth',
+    # new networks with whitening learned end-to-end
+    'rSfM120k-tl-resnet50-gem-w'        : 'http://cmp.felk.cvut.cz/cnnimageretrieval/data/networks/retrieval-SfM-120k/rSfM120k-tl-resnet50-gem-w-97bf910.pth',
+    'rSfM120k-tl-resnet101-gem-w'       : 'http://cmp.felk.cvut.cz/cnnimageretrieval/data/networks/retrieval-SfM-120k/rSfM120k-tl-resnet101-gem-w-a155e54.pth',
+    'rSfM120k-tl-resnet152-gem-w'       : 'http://cmp.felk.cvut.cz/cnnimageretrieval/data/networks/retrieval-SfM-120k/rSfM120k-tl-resnet152-gem-w-f39cada.pth',
+    'gl18-tl-resnet50-gem-w'            : 'http://cmp.felk.cvut.cz/cnnimageretrieval/data/networks/gl18/gl18-tl-resnet50-gem-w-83fdc30.pth',
+    'gl18-tl-resnet101-gem-w'           : 'http://cmp.felk.cvut.cz/cnnimageretrieval/data/networks/gl18/gl18-tl-resnet101-gem-w-a4d43db.pth',
+    'gl18-tl-resnet152-gem-w'           : 'http://cmp.felk.cvut.cz/cnnimageretrieval/data/networks/gl18/gl18-tl-resnet152-gem-w-21278d5.pth',
+}
+
+datasets_names = ['mapillary']
+whitening_names = []
+
+parser = argparse.ArgumentParser(description='PyTorch CNN Image Retrieval Testing')
+
+# network
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument('--network-path', '-npath', metavar='NETWORK',
+                    help="pretrained network or network path (destination where network is saved)")
+group.add_argument('--network-offtheshelf', '-noff', metavar='NETWORK',
+                    help="off-the-shelf network, in the format 'ARCHITECTURE-POOLING' or 'ARCHITECTURE-POOLING-{reg-lwhiten-whiten}'," + 
+                        " examples: 'resnet101-gem' | 'resnet101-gem-reg' | 'resnet101-gem-whiten' | 'resnet101-gem-lwhiten' | 'resnet101-gem-reg-whiten'")
+
+# test options
+parser.add_argument('--datasets', '-d', metavar='DATASETS', default='oxford5k,paris6k',
+                    help="comma separated list of test datasets: " + 
+                        " | ".join(datasets_names) + 
+                        " (default: 'oxford5k,paris6k')")
+parser.add_argument('--image-size', default=1024, type=int, metavar='N',
+                    help='maximum size of longer image side used for training (default: 1024)')
+parser.add_argument('--multiscale', '-ms', metavar='MULTISCALE', default='[1]', 
+                    help="use multiscale vectors for testing, " + 
+                    " examples: '[1]' | '[1, 1/2**(1/2), 1/2]' | '[1, 2**(1/2), 1/2**(1/2)]' (default: '[1]')")
+parser.add_argument('--whitening', '-w', metavar='WHITENING', default=None, choices=whitening_names,
+                    help="dataset used to learn whitening for testing: " + 
+                        " | ".join(whitening_names) + 
+                        " (default: None)")
+parser.add_argument('--generate-plot', default=False, type=bool, metavar='PLOT',
+                    help='Generates a plot over embedding distance and geographical distance')
+
+# GPU ID
+parser.add_argument('--gpu-id', '-g', default='0', metavar='N',
+                    help="gpu id used for testing (default: '0')")
+
+def main():
+    args = parser.parse_args()
+    imsize = args.image_size
+
+    # check if there are unknown datasets
+    for dataset in args.datasets.split(','):
+        if dataset not in datasets_names:
+            raise ValueError('Unsupported or unknown dataset: {}!'.format(dataset))
+
+    # setting up the visible GPU
+    os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu_id
+
+    # loading network from path
+    if args.network_path is not None:
+
+        print(">> Loading network:\n>>>> '{}'".format(args.network_path))
+        if args.network_path in PRETRAINED:
+            # pretrained networks (downloaded automatically)
+            state = load_url(PRETRAINED[args.network_path], model_dir=os.path.join(get_data_root(), 'networks'))
+        else:
+            # fine-tuned network from path
+            state = torch.load(args.network_path)
+
+        # parsing net params from meta
+        # architecture, pooling, mean, std required
+        # the rest has default values, in case that is doesnt exist
+        net_params = {}
+        net_params['architecture'] = state['meta']['architecture']
+        net_params['pooling'] = state['meta']['pooling']
+        net_params['local_whitening'] = state['meta'].get('local_whitening', False)
+        net_params['regional'] = state['meta'].get('regional', False)
+        net_params['whitening'] = state['meta'].get('whitening', False)
+        net_params['mean'] = state['meta']['mean']
+        net_params['std'] = state['meta']['std']
+        net_params['pretrained'] = False
+
+        # load network
+        net = init_network(net_params)
+        net.load_state_dict(state['state_dict'])
+        
+        # if whitening is precomputed
+        if 'Lw' in state['meta']:
+            net.meta['Lw'] = state['meta']['Lw']
+        
+        print(">>>> loaded network: ")
+        print(net.meta_repr())
+
+    # loading offtheshelf network
+    elif args.network_offtheshelf is not None:
+        
+        # parse off-the-shelf parameters
+        offtheshelf = args.network_offtheshelf.split('-')
+        net_params = {}
+        net_params['architecture'] = offtheshelf[0]
+        net_params['pooling'] = offtheshelf[1]
+        net_params['local_whitening'] = 'lwhiten' in offtheshelf[2:]
+        net_params['regional'] = 'reg' in offtheshelf[2:]
+        net_params['whitening'] = 'whiten' in offtheshelf[2:]
+        net_params['pretrained'] = True
+
+        # load off-the-shelf network
+        print(">> Loading off-the-shelf network:\n>>>> '{}'".format(args.network_offtheshelf))
+        net = init_network(net_params)
+        print(">>>> loaded network: ")
+        print(net.meta_repr())
+
+    # setting up the multi-scale parameters
+    ms = list(eval(args.multiscale))
+    if len(ms)>1 and net.meta['pooling'] == 'gem' and not net.meta['regional'] and not net.meta['whitening']:
+        msp = net.pool.p.item()
+        print(">> Set-up multiscale:")
+        print(">>>> ms: {}".format(ms))            
+        print(">>>> msp: {}".format(msp))
+    else:
+        msp = 1
+
+    # moving network to gpu and eval mode
+    net.cuda()
+    net.eval()
+    # set up the transform
+    resize = transforms.Resize((240,320), interpolation=2)
+    normalize = transforms.Normalize(
+        mean=net.meta['mean'],
+        std=net.meta['std']
+    )
+    transform = transforms.Compose([
+        resize,
+        transforms.ToTensor(),
+        normalize
+    ])
+    posDistThr = 25
+    negDistThr = 25
+    test_dataset = TuplesDataset(
+        name='mapillary',
+        mode='test',
+        imsize=imsize,
+        transform=transform,
+        posDistThr=posDistThr,
+        negDistThr=negDistThr,
+        tuple_mining='gps'
+    )
+    qidxs, pidxs = test_dataset.get_loaders()
+
+    opt = {'batch_size': 1, 'shuffle': False, 'num_workers': 8, 'pin_memory': True}
+
+    # evaluate on test datasets
+    datasets = datasets_names
+    for dataset in datasets: 
+        start = time.time()
+        
+        print('>> {}: Extracting...'.format(dataset))
+        
+        # Step 1: Extract Database Images - dbLoader
+        print('>> {}: Extracting Database Images...'.format(dataset))
+        dbLoader = torch.utils.data.DataLoader(
+            ImagesFromList(root='', images=[test_dataset.dbImages[i] for i in range(len(test_dataset.dbImages))], imsize=imsize, transform=transform),
+            **opt)        
+        poolvecs = torch.zeros(net.meta['outputdim'], len(test_dataset.dbImages)).cuda()
+        for i, input in enumerate(dbLoader):
+            poolvecs[:, i] = net(input.cuda()).data.squeeze()
+
+        # Step 2: Extract Query Images - qLoader
+        print('>> {}: Extracting Query Images...'.format(dataset))
+        qLoader = torch.utils.data.DataLoader(
+                ImagesFromList(root='', images=[test_dataset.qImages[i] for i in qidxs], imsize=imsize, transform=transform),
+                **opt)
+
+        qvecs = torch.zeros(net.meta['outputdim'], len(qidxs)).cuda()
+        for i, input in enumerate(qLoader):
+            qvecs[:, i] = net(input.cuda()).data.squeeze()
+        
+        # GPS: get query and pool coordinates
+        querycoordinates = torch.tensor([test_dataset.gpsInfo[test_dataset.qImages[i][-26:-4]] for i in qidxs], dtype=torch.float)
+        poolcoordinates = torch.tensor([test_dataset.gpsInfo[test_dataset.dbImages[i][-26:-4]] for i in range(len(test_dataset.dbImages))], dtype=torch.float)
+
+        # GPS: Compute distances
+        distances = torch.norm(querycoordinates[:, None] - poolcoordinates, dim=2)
+            
+        # GPS: Sort distances
+        distances, indicies = torch.sort(distances, dim=1, descending=False)
+        
+        # Step 3: Ranks 
+        scores = torch.mm(poolvecs.t(), qvecs)
+        #scores, ranks = torch.sort(scores, dim=0, descending=True) # Euclidan distance is 1 - Score 
+        
+        #ranks = ranks.cpu().numpy()
+        #ranks = np.transpose(ranks)
+
+        scores = scores.cpu().numpy()
+        scores = np.transpose(scores)
+
+        if args.generate_plot:
+            print('>>> {}: Generating Plot'.format(dataset))
+            gpsinfo = test_dataset.gpsInfo
+            k = 5
+            
+            for q in range(len(qidxs)):
+                k_indicies = indicies[q, :k]
+                print('K_Indicies', k_indicies)
+                k_dist = distances[q, :k]
+                print('K_distances', k_dist)
+                zero_score = scores[q, k_indicies[0]]
+                one_score = scores[q, k_indicies[0]]
+                print('0,1 Score:', zero_score, one_score)
+                
+
+
+            #np.savetxt("gps.csv", gpsdistances, delimiter=",")
+            #np.savetxt("embedding.csv", embeddingdistances, delimiter=",")
+        
+        print('>> {}: elapsed time: {}'.format(dataset, htime(time.time()-start)))
+
+def distance(query, positive):
+    return np.linalg.norm(np.array(query)-np.array(positive))
+
+if __name__ == '__main__':
+    main()
