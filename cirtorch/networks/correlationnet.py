7596a722db03e59def778a122180f2a4958da919
Holstrup <abho@dtu.dk>
2021-04-12 14:15:32 +0200

    refactored correlationnet

--- cirtorch/networks/correlationnet.py
+++ cirtorch/networks/correlationnet.py
@@ -38,6 +38,10 @@ WD = 4e-3
 network_path = 'data/exp_outputs1/mapillary_resnet50_gem_contrastive_m0.70_adam_lr1.0e-06_wd1.0e-06_nnum5_qsize2000_psize20000_bsize5_uevery5_imsize1024/model_epoch38.pth.tar'
 multiscale = '[1]'
 imsize = 1024
+posDistThr = 25
+negDistThr = 25
+query_size = 2000
+pool_size = 20000
 
 t = time.strftime("%Y-%d-%m_%H:%M:%S", time.localtime())
 tensorboard = SummaryWriter(f'data/correlation_runs/{INPUT_DIM}_{OUTPUT_DIM}_{t}')
@@ -45,20 +49,12 @@ tensorboard = SummaryWriter(f'data/correlation_runs/{INPUT_DIM}_{OUTPUT_DIM}_{t}
 """
 Dataset
 """
-tensor_meta = []
-def standardize(tensor, dimension, save=False):
-    global tensor_meta
+def standardize(tensor, dimension):
     means = tensor.mean(dim=dimension, keepdim=True)
     stds = tensor.std(dim=dimension, keepdim=True)
-    standardized_tensor = (tensor - means) / stds
-    means = means.cpu()
-    stds = stds.cpu()
-    if save:
-        tensor_meta = [means.numpy(), stds.numpy()]
-    return standardized_tensor
+    return (tensor - means) / stds
 
-
-def main():
+def load_placereg_net():
     # loading network from path
     if network_path is not None:
         state = torch.load(network_path)
@@ -97,59 +93,36 @@ def main():
         print(">>>> msp: {}".format(msp))
     else:
         msp = 1
+    return net
 
-    # moving network to gpu and eval mode
-    net.cuda()
-    net.eval()
-    # set up the transform
-    resize = transforms.Resize((240, 320), interpolation=2)
-    normalize = transforms.Normalize(
-        mean=net.meta['mean'],
-        std=net.meta['std']
-    )
-    transform = transforms.Compose([
-        resize,
-        transforms.ToTensor(),
-        normalize
-    ])
-    posDistThr = 25
-    negDistThr = 25
-    test_dataset = TuplesDataset(
-        name='mapillary',
-        mode='train',
-        imsize=imsize,
-        transform=transform,
-        posDistThr=posDistThr,
-        negDistThr=negDistThr,
-        tuple_mining='gps'
-    )
-    qidxs, pidxs = test_dataset.get_loaders()
+def load_dataloader(place_model, dataset, transform):
+    qidxs, pidxs = dataset.get_loaders()
 
     opt = {'batch_size': 1, 'shuffle': False, 'num_workers': 8, 'pin_memory': True}
 
     # Step 1: Extract Database Images - dbLoader
     dbLoader = torch.utils.data.DataLoader(
-          ImagesFromList(root='', images=[test_dataset.dbImages[i] for i in range(
-               len(test_dataset.dbImages))], imsize=imsize, transform=transform),
-          **opt)
-    poolvecs = torch.zeros(net.meta['outputdim'], len(test_dataset.dbImages)).cuda()
+          ImagesFromList(root='', images=[dataset.dbImages[i] for i in range(
+               len(dataset.dbImages))], imsize=imsize, transform=transform), **opt)
+    
+    poolvecs = torch.zeros(place_model.meta['outputdim'], len(dataset.dbImages)).cuda()
     for i, input in enumerate(dbLoader):
-            poolvecs[:, i] = net(input.cuda()).data.squeeze()
+            poolvecs[:, i] = place_model(input.cuda()).data.squeeze()
 
     # Step 2: Extract Query Images - qLoader
     qLoader = torch.utils.data.DataLoader(
             ImagesFromList(root='', images=[
-                           test_dataset.qImages[i] for i in qidxs], imsize=imsize, transform=transform), **opt)
+                           dataset.qImages[i] for i in qidxs], imsize=imsize, transform=transform), **opt)
 
-    qvecs = torch.zeros(net.meta['outputdim'], len(qidxs)).cuda()
+    qvecs = torch.zeros(place_model.meta['outputdim'], len(qidxs)).cuda()
     for i, input in enumerate(qLoader):
-            qvecs[:, i] = net(input.cuda()).data.squeeze()
+            qvecs[:, i] = place_model(input.cuda()).data.squeeze()
 
     # GPS: get query and pool coordinates
     querycoordinates = torch.tensor(
-            [test_dataset.gpsInfo[test_dataset.qImages[i][-26:-4]] for i in qidxs], dtype=torch.float)
-    poolcoordinates = torch.tensor([test_dataset.gpsInfo[test_dataset.dbImages[i][-26:-4]]
-                                    for i in range(len(test_dataset.dbImages))], dtype=torch.float)
+            [dataset.gpsInfo[dataset.qImages[i][-26:-4]] for i in qidxs], dtype=torch.float)
+    poolcoordinates = torch.tensor([dataset.gpsInfo[dataset.dbImages[i][-26:-4]]
+                                    for i in range(len(dataset.dbImages))], dtype=torch.float)
     
     # Dataset
     input_data = poolvecs.T
@@ -158,7 +131,6 @@ def main():
     input_data = standardize(input_data, 0)
     output_data = standardize(output_data, 0, save=True)
 
-    print(tensor_meta)
     input_data = input_data.cuda()
     output_data = output_data.cuda() 
     N, _ = output_data.size()
@@ -166,12 +138,10 @@ def main():
     print(input_data.size(), output_data.size())    
     
     torch_dataset = Data.TensorDataset(input_data, output_data)
-    train_set, val_set = torch.utils.data.random_split(torch_dataset, [N - N // 5, N // 5])    
     
-    train_loader = Data.DataLoader(dataset=train_set, batch_size=BATCH_SIZE, shuffle=True, num_workers=0,)
-    val_loader = Data.DataLoader(dataset=val_set, batch_size=BATCH_SIZE, shuffle=True, num_workers=0,)
+    data_loader = Data.DataLoader(dataset=torch_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=0,)
 
-    return train_loader, val_loader
+    return data_loader
 
 
 """
@@ -194,7 +164,7 @@ class CorrelationNet(torch.nn.Module):
         return x
 
 
-def plot_points(ground_truth, prediction, mode='Train'):
+def plot_points(ground_truth, prediction, mode, epoch):
     plt.clf()
     plt.scatter(ground_truth.data[:, 0].numpy(), ground_truth.data[:, 1].numpy(), color = "blue", alpha=0.2)
     plt.scatter(prediction.data[:, 0].numpy(), prediction.data[:, 1].numpy(), color = "red", alpha=0.2)
@@ -209,7 +179,7 @@ def plot_points(ground_truth, prediction, mode='Train'):
     image = transforms.ToTensor()(image).unsqueeze(0)
     tensorboard.add_image(f'Coordinates - {mode}', image[0], epoch)
 
-def plot_correlation(ground_truth, prediction, mode='Train'):
+def plot_correlation(ground_truth, prediction, mode, epoch):
     plt.clf()
     ground_truth = ground_truth.data.numpy()
     prediction = prediction.data.numpy()
@@ -230,10 +200,120 @@ def plot_correlation(ground_truth, prediction, mode='Train'):
     image = transforms.ToTensor()(image).unsqueeze(0)
     tensorboard.add_image(f'Correlation - {mode}', image[0], epoch)
 
+
+"""
+TRAINING
+"""
+
+def train(train_loader, place_model, net, criterion, optimizer, scheduler, epoch):
+    epoch_loss = 0
+    for step, (batch_x, batch_y) in enumerate(train_loader):
+        b_x = Variable(batch_x)
+        b_y = Variable(batch_y)
+
+        prediction = net(b_x)
+        loss = criterion(prediction, b_y)
+
+        epoch_loss += loss
+        loss.backward()         
+
+        if step == 1 and (epoch % (EPOCH // 100) == 0 or (epoch == (EPOCH-1))):
+            b_y = b_y.cpu()
+            prediction = prediction.cpu()
+            
+            plot_points(b_y, prediction, 'Train', epoch)
+            plot_correlation(b_y, prediction, 'Train', epoch) 
+ 
+    tensorboard.add_scalar('Loss/train', epoch_loss, epoch)
+
+    optimizer.step()
+    optimizer.zero_grad()
+    scheduler.step()
+
+def test(net, criterion, val_loader, epoch):
+    score = 0
+    net.eval()
+    for step, (batch_x, batch_y) in enumerate(val_loader):
+        prediction = net(batch_x)
+        score = criterion(prediction, batch_y)
+
+        if step == 0:
+            batch_y = batch_y.cpu()
+            prediction = prediction.cpu()
+            plot_points(batch_y, prediction, 'Validation')
+            plot_correlation(batch_y, prediction, 'Validation')  
+            tensorboard.add_scalar('Loss/validation', score, epoch)
+
+def main():
+    place_model = load_placereg_net()
+    net = CorrelationNet()
+
+    # moving network to gpu
+    place_model.cuda()
+    net.cuda()
+
+    # set up the transform
+    resize = transforms.Resize((240, 320), interpolation=2)
+    normalize = transforms.Normalize(
+        mean=net.meta['mean'],
+        std=net.meta['std']
+    )
+    transform = transforms.Compose([
+        resize,
+        transforms.ToTensor(),
+        normalize
+    ])
+    
+    train_dataset = TuplesDataset(
+        name='mapillary',
+        mode='train',
+        imsize=imsize,
+        nnum=1,
+        qsize=query_size,
+        poolsize=pool_size,
+        transform=transform,
+        posDistThr=posDistThr,
+        negDistThr=negDistThr, 
+        root_dir = 'data',
+        cities=''
+    )
+
+    val_dataset = TuplesDataset(
+            name='mapillary',
+            mode='val',
+            imsize=imsize,
+            nnum=1,
+            qsize=float('Inf'),
+            poolsize=float('Inf'),
+            transform=transform,
+            posDistThr=negDistThr, # Use 25 meters for both pos and neg
+            negDistThr=negDistThr,
+            root_dir = 'data',
+            cities=''
+    )
+
+    train_loader = load_dataloader(place_model, train_dataset, transform)
+    val_loader = load_dataloader(place_model, val_dataset, transform)
+
+    # Optimizer, scheduler and criterion
+    optimizer = torch.optim.Adam(net.parameters(), lr=LR, weight_decay=WD)
+    scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=math.exp(-0.01))
+    criterion = torch.nn.MSELoss().cuda()
+
+    losses = np.zeros(EPOCH)
+    for epoch in range(EPOCH):
+        print(f'====> {epoch}/{EPOCH}')
+        train(train_loader, place_model, net, criterion, optimizer, scheduler, epoch)
+
+        if (epoch % (EPOCH // 100) == 0 or (epoch == (EPOCH-1))):
+            with torch.no_grad():
+                test(net, criterion, val_loader, epoch)
+
+
+
+"""
 def local_correlation_plot(ground_truth, prediction, mode='Train', point=10):
     plt.clf()
-    #ground_truth = ground_truth.data.numpy()
-    #prediction = prediction.data.numpy()
 
     # Ground Truth
     distances = torch.norm(ground_truth - ground_truth[point], dim=1)
@@ -250,6 +330,7 @@ def local_correlation_plot(ground_truth, prediction, mode='Train', point=10):
         y = distances[i]
         correlated_points.append([x, y])
         i += 1
+    
     correlated_points = np.array(correlated_points)
     #correlated_points = correlated_points * tensor_meta[1] + tensor_meta[0] 
     plt.scatter(correlated_points[:, 1], correlated_points[:, 0])
@@ -262,71 +343,4 @@ def local_correlation_plot(ground_truth, prediction, mode='Train', point=10):
     image = PIL.Image.open(buf)
     image = transforms.ToTensor()(image).unsqueeze(0)
     tensorboard.add_image(f'Local Correlation point {point}- {mode}', image[0], epoch)
-
-
-def test(network, validation_loader):
-    score = 0
-    network.eval()
-    for step, (batch_x, batch_y) in enumerate(validation_loader):
-        prediction = network(batch_x)
-        score = loss_func(prediction, batch_y)
-
-        if step == 1:
-            batch_y = batch_y.cpu()
-            prediction = prediction.cpu()
-            plot_points(batch_y, prediction, 'Validation')
-            plot_correlation(batch_y, prediction, 'Validation')  
-            tensorboard.add_scalar('Loss/validation', score, epoch)
-
-
 """
-TRAINING
-"""
-# Dataset
-loader, val_loader = main()
-
-# Network
-net = CorrelationNet()
-optimizer = torch.optim.Adam(net.parameters(), lr=LR, weight_decay=WD)
-scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=math.exp(-0.01))
-loss_func = torch.nn.MSELoss()
-
-# Move to GPU
-net = net.cuda()
-loss_func = loss_func.cuda()
-
-# Train loop
-losses = np.zeros(EPOCH)
-for epoch in range(EPOCH):    
-    epoch_loss = 0
-    for step, (batch_x, batch_y) in enumerate(loader):
-
-        b_x = Variable(batch_x)
-        b_y = Variable(batch_y)
-
-        prediction = net(b_x)
-
-        loss = loss_func(prediction, b_y)
-        epoch_loss += loss
- 
-        loss.backward()         
-
-        if step == 1 and (epoch % (EPOCH // 100) == 0 or (epoch == (EPOCH-1))):
-            b_y = b_y.cpu()
-            prediction = prediction.cpu()
-            
-            plot_points(b_y, prediction, 'Train')
-            plot_correlation(b_y, prediction, 'Train')
-            local_correlation_plot(b_y, prediction, 'Train', 10)
-            local_correlation_plot(b_y, prediction, 'Train', 42) 
-            local_correlation_plot(b_y, prediction, 'Train', 58)
-            local_correlation_plot(b_y, prediction, 'Train', 91)     
- 
-    tensorboard.add_scalar('Loss/train', epoch_loss, epoch)
-
-    if (epoch % (EPOCH // 100) == 0 or (epoch == (EPOCH-1))):
-        test(net, val_loader)
-
-    optimizer.step()
-    optimizer.zero_grad()
-    scheduler.step()
