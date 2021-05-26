99fd65132e5318ba74d33c6699f122f610e52ab7
Holstrup <abho@dtu.dk>
2021-05-26 12:40:34 +0200

    iouextract

---
+++ cirtorch/utils/iouextraction.py
@@ -0,0 +1,52 @@
+import sys
+sys.path.insert(0, "/Users/alexanderholstrup/git/VisualPlaceRecognition/cnnimageretrieval-pytorch")
+
+import torchvision.transforms as transforms
+from cirtorch.datasets.traindataset import TuplesDataset
+from cirtorch.utils.view_angle import field_of_view, ious, plot_fov
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import json
+from os.path import join
+
+MODE = 'train'
+root = 'data'
+
+default_cities = {
+    'train': ["zurich", "london", "boston", "melbourne", "amsterdam","helsinki",
+              "tokyo","toronto","saopaulo","moscow","trondheim","paris","bangkok",
+              "budapest","austin","berlin","ottawa","phoenix","goa","amman","nairobi","manila"],
+    'val': ["cph", "sf"],
+    'test': ["miami","athens","buenosaires","stockholm","bengaluru","kampala"]
+}
+
+train_dataset = TuplesDataset(
+        name='mapillary',
+        mode='train',
+        qsize=float('Inf'),
+        poolsize=float('Inf'),
+        posDistThr=50,
+        negDistThr=50, 
+        root_dir = root,
+        cities='zurich',
+    )
+
+data = {}
+for city in default_cities[MODE]:
+    for i, qIdx in enumerate(train_dataset.qpool):
+        # qID from qIdx
+        qId = train_dataset.qImages[qIdx][-26:-4]
+            
+        # positives from index i
+        positives = train_dataset.ppool[i]
+        dbIds = [train_dataset.dbImages[j][-26:-4] for j in positives]
+
+        # get coordinates
+        points = [train_dataset.gpsInfo[qId] + train_dataset.angleInfo[qId]]
+        points.extend([train_dataset.gpsInfo[dbId] + train_dataset.angleInfo[dbId] for dbId in dbIds])
+
+        pol = field_of_view(points)
+        data[qId] = [dbIds, ious(pol[0], pol[1:])]
+    json.dump(data, open(join(root, 'train_val', city, 'query', 'ious.json'), 'w+'))
